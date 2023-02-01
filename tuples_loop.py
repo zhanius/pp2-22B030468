ba428bc0fbe5573b186e2e688d35503070d9dbaa
zhanius <118667628+zhanius@users.noreply.github.com>
2023-02-02 00:56:25 +0600

    Delete tuples_loop.py

--- tuples_loop.py
+++
@@ -1,9 +0,0 @@
-thistuple = ("apple", "banana", "cherry")
-for x in thistuple:
-  print(x)
-
-#index
-thistuple = ("apple", "banana", "cherry")
-for i in range(len(thistuple)):
-  print(thistuple[i])
-
