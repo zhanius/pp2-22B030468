5c965a97253db16e7ccbca9c66ca4bdd159876a9
zhanius <118667628+zhanius@users.noreply.github.com>
2023-03-28 17:31:58 +0600

    mickey

--- TSIS7/mickeyclock.py
+++ TSIS7/mickeyclock.py
@@ -1,18 +1,53 @@
-import pygame 
+import pygame
 from datetime import datetime
+import math
 
+
+#initialization
 pygame.init()
-w, h = 1200, 800
-surface = pygae.display.set_mode((w,h))
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+    
+#to get current time
+time = datetime.now()
+sec = time.second
+min = time.minute
+running = True
 
-curr_time = datetime.now()
-curr_sec = datetime.now.second()
-curr_min = fatetime.now.minute()
- 
+
+#load. images 
+clock_image = pygame.transform.scale(pygame.image.load('mickeyclock.jpeg'), (800, 600))
+sechand_image = pygame.image.load('sec.png')
+sechand_image = pygame.transform.scale(sechand_image, (250, 75))
+sechand_rect = sechand_image.get_rect()
+sechand_rect.center = (400, 300)
+minhand_image = pygame.image.load('min.png')
+minhand_image = pygame.transform.scale(minhand_image, (200, 50))
+minhand_rect = minhand_image.get_rect()
+minhand_rect.center = (400, 300)
+
+
+
 while True:
-    surface.fill((255,255,255))
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+         if event.type == pygame.QUIT:
             exit()
+    screen.fill((0,0,0))
+    screen.blit(clock_image, (0, 0))
+    
+    rot_minhand = pygame.transform.rotate(minhand_image, -1 * (6 * min) - 160)
+    rot_minhand_rect = rot_minhand.get_rect()
+    rot_minhand_rect.center = minhand_rect.center
+    screen.blit(rot_minhand, rot_minhand_rect)
+    
+    rot_sechand = pygame.transform.rotate(sechand_image, -1 * (6 * sec) + 90)
+    rot_sechand_rect =rot_sechand.get_rect()
+    rot_sechand_rect.center = sechand_rect.center
+    screen.blit(rot_sechand, rot_sechand_rect)
 
+    time = datetime.now()
+    sec = time.second
+    min = time.minute
+        
+    pygame.display.update()
+    clock.tick(60)
